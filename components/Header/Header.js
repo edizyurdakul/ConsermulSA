776161fa0c12d476f5775aecc2408954422ee6b0
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-10 20:24:58 +0200

    Desktop navigation complete, mobile navigation WIP

---
+++ components/Header/Header.js
@@ -0,0 +1,37 @@
+import NextLink from "next/link";
+import styles from "./Header.module.scss";
+import { useState } from "react";
+
+const Header = () => {
+  const [animated, setAnimated] = useState(false);
+  console.log(animated);
+  const toggleAnimated = () => {
+    setAnimated(!animated);
+  };
+  return (
+    <header className={styles.header}>
+      <NextLink href="">Name - Logo</NextLink>
+      <nav>
+        <div className={styles.menu}></div>
+        <div className={styles.navigation}>
+          <ul>
+            <li>
+              <NextLink href="">Menu Item 1</NextLink>
+            </li>
+            <li>
+              <NextLink href="">Menu Item 2</NextLink>
+            </li>
+            <li>
+              <NextLink href="">Menu Item 3</NextLink>
+            </li>
+            <li>
+              <NextLink href="">Menu Item 4</NextLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import { Header } from "../components/Header";
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
     </div>
   );
