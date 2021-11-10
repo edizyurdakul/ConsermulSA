619098d3fb738155721ff5b4f724916dd5c8a3ac
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-11 01:23:32 +0200

    Mobile menu implemented, testing

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,28 +1,59 @@
 import NextLink from "next/link";
 import styles from "./Header.module.scss";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+    console.log(openMenu);
+  };
+
   return (
     <header className={styles.header}>
       <NextLink href="">Name - Logo</NextLink>
       <nav>
-        <div className={styles.menu}></div>
-        <div className={styles.navigation}>
-          <ul>
-            <li>
-              <NextLink href="">Menu Item 1</NextLink>
-            </li>
-            <li>
-              <NextLink href="">Menu Item 2</NextLink>
-            </li>
-            <li>
-              <NextLink href="">Menu Item 3</NextLink>
-            </li>
-            <li>
-              <NextLink href="">Menu Item 4</NextLink>
-            </li>
-          </ul>
+        <div
+          className={
+            openMenu == true
+              ? `${styles.menu} ${styles.animate}`
+              : `${styles.menu}`
+          }
+          onClick={() => {
+            toggleMenu();
+          }}
+        ></div>
+        <div
+          className={
+            openMenu == true
+              ? `${styles.navigationWrapper} ${styles.animated}`
+              : `${styles.navigationWrapper}`
+          }
+        >
+          <div
+            className={
+              openMenu == true
+                ? `${styles.navigation} ${styles.animated}`
+                : `${styles.navigation}`
+            }
+          >
+            <ul>
+              <li>
+                <NextLink href="">Menu Item 1</NextLink>
+              </li>
+              <li>
+                <NextLink href="">Menu Item 2</NextLink>
+              </li>
+              <li>
+                <NextLink href="">Menu Item 3</NextLink>
+              </li>
+              <li>
+                <NextLink href="">Menu Item 4</NextLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { Header } from "../components/Header";
+import { motion } from "framer-motion";
+
 const HomePage = () => {
   return (
     <div>
       <Header />
       <h1>Hello World</h1>
+      <br />
     </div>
   );
 };
