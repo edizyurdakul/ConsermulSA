694461ddc7936bb1e726565200432d25911f9e21
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-15 13:31:03 +0200

    Social icons added to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import styles from "./Footer.module.scss";
 import Link from "next/link";
+import { Facebook, Twitter, Instagram } from "../Icons";
 
 const Footer = () => {
   return (
@@ -8,15 +9,35 @@ const Footer = () => {
         <h3>Name - Logo</h3>
         <ul>
           <li>
-            <Link href="">About our company</Link>
-            <Link href="">Our previous works</Link>
+            <h4>COMPANY</h4>
+            <Link href="#">About our company</Link>
+            <Link href="#">Our previous works</Link>
           </li>
           <li>
-            <Link href="">Services we provide</Link>
-            <Link href="">Request a quote</Link>
+            <h4>SERVICES</h4>
+            <Link href="#">Services we provide</Link>
+            <Link href="#">Request a quote</Link>
           </li>
           <li>
-            <Link href="">Contact</Link>
+            <h4>HELP</h4>
+            <Link href="#">Contact</Link>
+            <div className={styles.social}>
+              <Link href="#">
+                <div>
+                  <Facebook />
+                </div>
+              </Link>
+              <Link href="#">
+                <div>
+                  <Twitter />
+                </div>
+              </Link>
+              <Link href="#">
+                <div>
+                  <Instagram />
+                </div>
+              </Link>
+            </div>
           </li>
         </ul>
       </div>
