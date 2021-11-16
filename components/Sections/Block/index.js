a0ffbdf5ccf9e62a12d33566aee29eeb3a011927
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-16 17:30:47 +0200

    finish informational block for about page

--- components/Sections/Block/index.js
+++ components/Sections/Block/index.js
@@ -1,7 +1,24 @@
 import styles from "./Block.module.scss";
+import NextImage from "next/image";
 
-const Block = () => {
-  return <div></div>;
+const Block = ({ order }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.heroImage}>
+            <NextImage src={"/placeholder.png"} layout="fill" />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <p>
+            Provide your customers a story they would enjoy keeping in mind the objectives of your website. Pay special
+            attention to the tone of voice. Try to win the customers' trust by being positive
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Block;
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,13 @@
-import { HomeAbout, CallToAction, FAQ, Team, CommonHero } from "../components/Sections";
+import { HomeAbout, CallToAction, FAQ, Team, CommonHero, Block } from "../components/Sections";
 
 const AboutPage = () => {
   return (
     <main>
       <section>
         <CommonHero />
+      </section>
+      <section>
+        <Block order="order" />
       </section>
     </main>
   );
