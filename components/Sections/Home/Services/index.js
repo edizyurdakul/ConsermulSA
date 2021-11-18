286514e597abd78db7a8cff06fca3d54c8fbc80b
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-18 23:31:38 +0200

    Services section animation complete

--- components/Sections/Home/Services/index.js
+++ components/Sections/Home/Services/index.js
@@ -1,46 +1,87 @@
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import styles from "./Services.module.scss";
 
-import React from "react";
-
 const Services = () => {
+  const ul = {
+    hidden: { opacity: 0, y: 36 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.35,
+        staggerChildren: 0.25,
+      },
+    },
+  };
+
+  const li = {
+    hidden: { opacity: 0, y: 36 },
+    show: { opacity: 1, y: 0 },
+  };
+  const heading = {
+    hidden: { opacity: 0, y: 36 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.25,
+      },
+    },
+  };
+
+  const animation = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("show");
+    }
+    if (!inView) {
+      animation.start("hidden");
+    }
+  }, [inView]);
   return (
     <>
       <div className={styles.wrapper}>
-        <div className={styles.contentWrapper}>
-          <h2>Services</h2>
+        <div ref={ref} className={styles.contentWrapper}>
+          <motion.h2 variants={heading} animate={animation}>
+            Services
+          </motion.h2>
           <div className={styles.content}>
-            <ul>
-              <li>
+            <motion.ul variants={ul} animate={animation}>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>First Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>Second Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>Third Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>Fourth Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>Fifth Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={li}>
                 <div>Icon</div>
                 <h3>Sixth Service</h3>
                 <p>Short text describing a feature of your service. Think how it can benefit your potential customer</p>
-              </li>
-            </ul>
+              </motion.li>
+            </motion.ul>
           </div>
         </div>
       </div>
