ac7786faab1d673f41322e62829eca3756a0c9b2
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-11-19 20:32:02 +0200

    Complete animations on block and reverse block components

--- components/Sections/Block/index.js
+++ components/Sections/Block/index.js
@@ -2,17 +2,45 @@ import styles from "./Block.module.scss";
 import NextImage from "next/image";
 
 export const Block = ({ title, paragraph }) => {
+  const ul = {
+    hidden: { opacity: 0, y: 36 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.35,
+        staggerChildren: 0.25,
+      },
+    },
+  };
+
+  const li = {
+    hidden: { opacity: 0, y: 36 },
+    show: { opacity: 1, y: 0 },
+  };
+
+  const animation = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("show");
+    }
+    if (!inView) {
+      animation.start("hidden");
+    }
+  }, [inView]);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.left}>
+      <div ref={ref} variants={ul} animate={animation} className={styles.container}>
+        <div variants={li} className={styles.left}>
           <div className={styles.heroImage}>
             <NextImage src={"/placeholder.png"} layout="fill" />
           </div>
         </div>
-        <div className={styles.right}>
+        <div variants={li} className={styles.right}>
           <h2>{title}</h2>
-
           <p>{paragraph}</p>
         </div>
       </div>
@@ -21,14 +49,43 @@ export const Block = ({ title, paragraph }) => {
 };
 
 export const BlockReverse = ({ title, paragraph }) => {
+  const ul = {
+    hidden: { opacity: 0, y: 36 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.35,
+        staggerChildren: 0.25,
+      },
+    },
+  };
+
+  const li = {
+    hidden: { opacity: 0, y: 36 },
+    show: { opacity: 1, y: 0 },
+  };
+
+  const animation = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("show");
+    }
+    if (!inView) {
+      animation.start("hidden");
+    }
+  }, [inView]);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.left}>
+      <div ref={ref} variants={ul} animate={animation} className={styles.container}>
+        <div variants={li} className={styles.left}>
           <h2>{title}</h2>
           <p>{paragraph}</p>
         </div>
-        <div className={`${styles.right} ${styles.reverse}`}>
+        <div variants={li} className={`${styles.right} ${styles.reverse}`}>
           <div className={styles.heroImage}>
             <NextImage src={"/placeholder.png"} layout="fill" />
           </div>
